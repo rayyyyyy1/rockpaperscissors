e82d9897c3dd4ed205068605b812105f0705096d
rayyyyyy1 <cojocarucristian65@gmail.com>
2022-10-31 12:09:47 +0200

    gata da imi e lene sa fac css-ul

--- javascript.js
+++ javascript.js
@@ -1,38 +1,54 @@
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection==computerSelection)
+const choises=["rock","paper","scissors"]
+let wins=0;
+let losses=0;
+
+startGame();
+
+function playRound(playerSelection) {
+    let computerChoice=getComputerChoice();
+    let result= rps( playerSelection , computerChoice );
+    if(result=="win")
         wins++;
-        losses++;
-    if (playerSelection=="rock")
-        if (computerSelection=="paper")
-            losses++;
-            else wins++;
-    if (playerSelection=="paper")
-        if (computerSelection=="scissors")
-            losses++;
-            else wins++;
-    if (playerSelection=="scissors")
-        if (computerSelection=="rock")
-            losses++;
-            else wins++;
-    document.querySelector('.score');
-    const scoretext=document.createElement('p');
+        else if(result=="lost")
+                losses++;
+    scoretext = document.querySelector('p');
     scoretext.textContent=`The score is ${wins}:${losses}`;
-    console.log(wins);
-        
+    checkWinner(wins,losses);
   }
+
+function rps(playerSelection,computerChoice){
+    console.log(playerSelection, computerChoice)
+    if (playerSelection==computerChoice)
+        return "tie";
+        else if ((playerSelection=="rock" && computerChoice=="scissors") || 
+        (playerSelection=="paper" && computerChoice=="rock")||
+        (playerSelection=="scissors" && computerChoice=="paper"))
+                return "win"; 
+                else return "lost";
+} 
+
+  function checkWinner(wins,losses){
+    if(wins==5){
+        document.querySelector('p').textContent='You won!!!';
+        resetGame();
+    }
+    if(losses==5){
+        document.querySelector('p').textContent='You Lost :(';
+        resetGame();
+    }
+  }
+function resetGame(){
+  wins=0;
+  losses=0;
+}
 
   function getComputerChoice(){
     return choises[Math.floor(Math.random()*choises.length)];
   }
   
   function startGame(){
-    let btn = document.querySelector(".button");
-    console.log(btn.id);
-    //btn.addEventListener('click', console.log(this));
-       // btn.forEach((alegere) =>{
-       // alegere.addEventListener(('click'),() =>{console.log(alegere.id);playRound(alegere.id, getComputerChoice);})})
+    
+    let btn = document.querySelectorAll(".button");
+    btn.forEach((alegere) =>{
+      alegere.addEventListener(('click'),() =>{playRound(alegere.id);})})
 }
-startGame();
-let wins=0;
-let losses=0;
-const choises=["rock","paper","scissors"];
